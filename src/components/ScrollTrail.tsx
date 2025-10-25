919918356bd30d6e220d4c8f10119aafeea16ba2
didgmltmd <gomboy11@naver.com>
2025-10-25 19:45:53 +0900

    motion.div 동작 공부

--- src/components/ScrollTrail.tsx
+++ src/components/ScrollTrail.tsx
@@ -1,33 +1,63 @@
-import {motion} from "framer-motion"
-import { useState } from "react";
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, Sphere } from "@react-three/drei";
+import { pointsInner, pointsOuter } from "./utils.js";
 
-export default function ScrollTrail(){
-    const buttonVariants = {
-            // function으로 정의하는 모습
-            hover: (clicked) => ({
-            // 클릭된 버튼은 scale이 커지지 않는다.
-            scale: clicked ? 1 : 1.5,
-            }),
-            pressed: {
-                scale: 0.5,
-            },
-            rest: {
-                scale: 0.5,
-            },
-        };
+const ParticleRing = () => {
+  return (
+    <div className="relative">
+      <Canvas
+        camera={{
+          position: [10, -7.5, -5],
+        }}
+        style={{ height: "100vh" }}
+        className="bg-slate-900"
+      >
+        <OrbitControls maxDistance={20} minDistance={10} />
+        <directionalLight />
+        <pointLight position={[-30, 0, -30]} power={10.0} />
+        <PointCircle />
+      </Canvas>
 
-    const [clicked, setClicked] = useState(false);
-    return(
-            <motion.button
-            initial="rest"
-            whileHover="hover" // hover상태 일 때 hover animation발생
-            whileTap="pressed" 
-            variants={buttonVariants}
-            custom={clicked} // custom을 통해 값을 전달 할 수 있다. 
-            onClick={() => setClicked(true)}
-            >
-            Click me!
-            </motion.button>
-    )
+      <h1 className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] text-slate-200 font-medium text-2xl md:text-5xl pointer-events-none">
+        Drag & Zooms
+      </h1>
+    </div>
+  );
+};
 
-}
+const PointCircle = () => {
+  const ref = useRef(null);
+
+  useFrame(({ clock }) => {
+    if (ref.current?.rotation) {
+      ref.current.rotation.z = clock.getElapsedTime() * 0.05;
+    }
+  });
+
+  return (
+    <group ref={ref}>
+      {pointsInner.map((point) => (
+        <Point key={point.idx} position={point.position} color={point.color} />
+      ))}
+      {pointsOuter.map((point) => (
+        <Point key={point.idx} position={point.position} color={point.color} />
+      ))}
+    </group>
+  );
+};
+
+const Point = ({ position, color }) => {
+  return (
+    <Sphere position={position} args={[0.1, 10, 10]}>
+      <meshStandardMaterial
+        emissive={color}
+        emissiveIntensity={0.5}
+        roughness={0.5}
+        color={color}
+      />
+    </Sphere>
+  );
+};
+
+export default ParticleRing;
